18e3ed55f303f06aa638a2b7d06c0ef3b922f097
lgabci <gl12qw@gmail.com>
2020-10-09 00:19:53 +0200

    13

--- freebsd/cf.sh
+++ freebsd/cf.sh
@@ -1,22 +1,44 @@
 #!/bin/sh
 
+set -eu
+
+PNAME=$(basename $0)
+
 case $# in
   2)
     fname="$1"
+    exists=""
     rights=""
     content="$2"
     ;;
   3)
     fname="$1"
-    rights="$2"
+    exists="$2"
+    rights=""
     content="$3"
+    ;;
+  4)
+    fname="$1"
+    exists="$2"
+    rights="$3"
+    content="$4"
     ;;
   *)
     echo "Usage:" >&2
-    echo "  $(basename $0) filename [rights] content" >&2
+    echo "  $PNAME filename [exists [rights]] content" >&2
     echo "  filename: the file to be written" >&2
-    echo "  rights:   for chmod" >&2
+    echo "  exists:   a = append existing file, n = new file, empty or - = doesn't matter" >&2
+    echo "  rights:   for chmod (empty or - = no chmod run)" >&2
     echo "  content:  content into the file" >&2
+    exit 1
+    ;;
+esac
+
+case $rights in
+  ""|-|[0-7]|[0-7][0-7]|[0-7][0-7][0-7]|[0-7][0-7][0-7][0-7]|[-+][rwx]|[ugoa][-+][rwx])
+    ;;
+  *)
+    echo "$PNAME: bad rigths value: \"$rights\"" >&2
     exit 1
     ;;
 esac
