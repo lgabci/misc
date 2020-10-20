323907ec4bd45e9afa69df357c4aa8d9d28c8e3c
lgabci <gl12qw@gmail.com>
2020-10-20 23:34:21 +0200

    15

--- freebsd/install-user.sh
+++ freebsd/install-user.sh
@@ -13,5 +13,6 @@ fi
 
 git config --global user.email "gl12qw@gmail.com"
 git config --global user.name "lgabci"
+git config --global pull.rebase false
 
 git remote set-url origin git@github.com:lgabci/misc.git
