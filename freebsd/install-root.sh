87b5a297f6e02bbc9a204b08ebda16e0321191e0
lgabci <gl12qw@gmail.com>
2020-10-31 22:19:42 +0100

    19

--- freebsd/install-root.sh
+++ freebsd/install-root.sh
@@ -17,6 +17,7 @@ freebsd-update install || q=$?
 if [ ${q:-0} != 2 ]; then
   poweroff
 fi
+unset q
 
 # set rc.conf
 sysrc local_unbound_enable=YES
@@ -29,61 +30,24 @@ sysrc slim_enable=YES
 
 # install packages
 pkg install -y xorg icewm slim sudo vim bash bash-completion
-
-# install etc files
-if ! [ -e /usr/local/etc/sudoers.d/wheel ]; then
-  echo "# allow members of group wheel to execute any command
-%wheel ALL=(ALL) ALL
-
-# allow member of group wheel to power off without password
-%wheel ALL=(ALL) NOPASSWD: /sbin/shutdown, /sbin/reboot" \
-    >/usr/local/etc/sudoers.d/wheel
-
-  chmod 600 /usr/local/etc/sudoers.d/wheel
-fi
 fi ##########################################################
 
-# wlan devices
-wpa=n
-wlandevs=$(sysctl -n net.wlan.devices)
-for d in $wlandevs; do
-  case "$d" in
-    ath0)
-      wpa=y
-      a="$(./getcont.sh /etc/rc.conf "$d")"
-      ./cf.sh /home/gabci/rc.conf a - "$a"
-      unset a
-
-      sysrc keymap=hu.102
-      sysrc wlans_ath0="wlan0"
-      sysrc ifconfig_wlan0="DHCP WPA"
-      sysrc create_args_wlan0="country HU regdomain ETSI"
+while read src trg mod; do
+  case "$src" in
+    *.append)
+      src="$(basename "$src")"
+      echo "APPEND $src --> $trg"
+      ;;
+    *)
+      echo "new    $src --> $trg"
       ;;
   esac
-done
+done <files/root.txt
 
-a="$(./getcont.sh /etc/rc.conf-2 "$d")"
-./cf.sh /home/gabci/rc.conf a - "$a"
-unset a
 
-exit ## -------------------------------
 
-if [ "$wpa" = y ]; then
-  if ! [ -e /etc/wpa_supplicant.conf ]; then
-    echo "network={
-  ssid="omega"
-  priority=10
-  proto=RSN
-  key_mgmt=WPA-PSK
-  pairwise=CCMP
-  group=CCMP
-  psk="password"
-}" >/etc/wpa_supplicant.conf
-    chmod 600 /etc/wpa_supplicant.conf
-  fi
-fi
+exit #######################################################
 
-    /etc/login.conf
 cap_mkdb /etc/login.conf
 pw user mod root -L american
 pw user mod gabci -L american
