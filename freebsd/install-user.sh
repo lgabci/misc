f5b3812727ca471a631ee067cf7c37427ee3343f
lgabci <gl12qw@gmail.com>
2020-12-09 23:43:48 +0100

    29

--- freebsd/install-user.sh
+++ freebsd/install-user.sh
@@ -12,9 +12,14 @@ find "$DIR/" -type f | while read src; do
       trg=${trg%.append}
       trg=$HOME/${trg#/home/}
       lines=$(wc -l <"$src")
-      if ! tail -n "$lines" "$trg" | diff "$src" - >/dev/null; then
-        echo "APPEND $src -> $trg"  ######
-        cat "$src" >>"$trg"
+      if [ -e "$trg" ]; then
+        if ! tail -n "$lines" "$trg" | diff "$src" - >/dev/null; then
+          echo "APPEND $src -> $trg"  ######
+          cat "$src" >>"$trg"
+        fi
+      else
+        echo "File not found: $trg" >&2
+	exit 1
       fi
       ;;
     /home/*)
@@ -36,4 +41,18 @@ if [ ! -e "$HOME/.login_conf.db" ]; then
   cap_mkdb $HOME/.login_conf
 fi
 
-## git remote set-url origin git@github.com:lgabci/misc.git
+giturl="git@github.com:lgabci/misc.git"
+git remote -v show | \
+  while read name url type; do
+    if [ "$name" = origin ] && [ "$url" != "$giturl" ]; then
+      type=${type#(}
+      type=${type%)}
+      unset t
+      case "$type" in
+        push)
+          t=--push
+          ;;
+      esac
+      git remote set-url ${t:-} "$name" "$giturl"
+    fi
+  done
