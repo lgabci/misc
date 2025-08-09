c55c623b75940ad5263bb7e780e5cfd747bde6c3
lgabci <gl12qw@gmail.com>
2025-08-09 22:15:56 +0200

    build-project.sh

--- home/bin/build-project.sh
+++ home/bin/build-project.sh
@@ -2,15 +2,17 @@
 
 set -eu
 
-if tdir=$(git rev-parse --show-toplevel); then
+basename=$(basename $0)
+
+if tdir=$(git rev-parse --show-toplevel 2>/dev/null); then
   comp="$tdir/compile.sh"
   if [ -x "$comp" ]; then
-    "$tdir/compile.sh" $@
+    "$comp" $@
   else
-    echo "No executable compile.sh found in git top level directory." >&2
+    echo "$basename: No executable compile.sh found in git top level directory." >&2
     exit 1
   fi
 else
-  echo "Not in a git working tree." >&2
+  echo "$basename: Not in a git working tree." >&2
   exit 1
 fi
