43b4a6688422c03d1d9780ffb99b7b783f675f72
lgabci <gl12qw@gmail.com>
2025-12-14 10:05:31 +0100

    build-project.sh: rename compile.sh to build.sh

--- home/bin/build-project.sh
+++ home/bin/build-project.sh
@@ -3,13 +3,14 @@
 set -eu
 
 basename=$(basename $0)
+build="build.sh"
 
 if tdir=$(git rev-parse --show-toplevel 2>/dev/null); then
-  comp="$tdir/compile.sh"
-  if [ -x "$comp" ]; then
-    "$comp" $@
+  fullbuild="$tdir/$build"
+  if [ -x "$fullbuild" ]; then
+    "$fullbuild" $@
   else
-    echo "$basename: No executable compile.sh found in git top level directory." >&2
+    echo "$basename: No executable $build found in git top level directory." >&2
     exit 1
   fi
 else
